d0f531c3b9b4d19993e5afa6488a84ea48ef1340
nznrlln <102247982+nznrlln@users.noreply.github.com>
2022-11-12 20:33:54 +0300

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Нияз Нуруллин on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
